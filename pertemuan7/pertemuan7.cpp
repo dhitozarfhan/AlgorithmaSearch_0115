622090515f4eae564fae2a3ba4942a076a488db6
dhitozarfhan <dhitozarfhan@gmail.com>
2024-04-05 10:04:11 +0700

    jumlah perbandingan

--- pertemuan7/pertemuan7.cpp
+++ pertemuan7/pertemuan7.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int arr{ 20 }; //Array to be searched
+int arr[20];    //Array to be searched
 int n;         //Number of elements in the array
-int i;         //Index of array element
+int i;        //Index of array element
 
 void input()
 {
 	while (true)
 	{
-		cout << "Enter the number of elements in the array: ";
+		cout << "Enter The number of element in the array : ";
 		cin >> n;
 		if ((n > 0) && (n <= 20))
 			break;
 		else
-			cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
 
 	// Accept array elements 
-	cout << "\n--------------------\n";
-	cout << " Enter array elements \n";
-	cout << "--------------------\n";
+	cout << "\n----------------------\n";
+	cout << "Enter Element Array\n";
+	cout << "------------------------\n";
 	for (i = 0; i < n; i++)
 	{
 		cout << "<" << (i + 1) << ">";
 		cin >> arr[i];
 	}
 }
+
+void linearsearch()
+{
+	char ch;
+	int ctr; //Number of comporisons
+	int item;
+
+	
