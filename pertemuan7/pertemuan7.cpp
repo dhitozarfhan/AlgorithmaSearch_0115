6650f3c989720d731cdfb5951a182a9a3c1109b4
dhitozarfhan <dhitozarfhan@gmail.com>
2024-04-05 09:46:50 +0700

    menerima elemen array

--- pertemuan7/pertemuan7.cpp
+++ pertemuan7/pertemuan7.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 
 int arr{ 20 }; //Array to be searched
-int n;         //Number of element in the array
+int n;         //Number of elements in the array
 int i;         //Index of array element
 
 void input()
 {
 	while (true)
 	{
-		cout << "Enter the number of element in the array: ";
+		cout << "Enter the number of elements in the array: ";
 		cin >> n;
 		if ((n > 0) && (n <= 20))
 			break;
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
 	}
+
+	// Accept array elements 
+	cout << "\n--------------------\n";
+	cout << " Enter array elements \n";
+	cout << "--------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
